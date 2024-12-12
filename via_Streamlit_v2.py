79bf348ebb3a0b82f0ca54b6670c2f1c5b538e31
cabana41 <cabana41@gmail.com>
2024-12-12 17:00:10 +0900

    Update via_Streamlit_v2.py

--- via_Streamlit_v2.py
+++ via_Streamlit_v2.py
@@ -187,7 +187,11 @@ def survey_page():
     col2.metric("투자 성향:", investment_type)
     col3.metric("투자 기간:", st.session_state.user_horizon or "**미선택**")
     
-    if st.button("포트폴리오 보기 🚀"):
+    if "button_clicked" not in st.session_state:
+    st.session_state.button_clicked = False
+
+    if st.button("포트폴리오 보기 🚀") or st.session_state.button_clicked:
+        st.session_state.button_clicked = True
         if st.session_state.user_risk and st.session_state.user_horizon:
             go_to_page("portfolio")
         else:
